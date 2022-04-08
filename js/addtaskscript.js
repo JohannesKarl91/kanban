bc1729cb4a4a604b9a3a6acc108dc206d5e7c2d8
Rebecca2106 <you@example.com>
2022-04-08 15:34:38 +0200

    update-css

--- js/addtaskscript.js
+++ js/addtaskscript.js
@@ -1,3 +1,19 @@
+let allTasks=[];
+
 function addTask(){
-    
+    let title = document.getElementById('title');
+    let date= document.getElementById('date');
+    let category = document.getElementById('category');
+    let description = document.getElementById('description');
+    let urgency = document.getElementById('urgency')
+
+
+    let task={
+        'title': title,
+        'category': category,
+        'description': description,
+        'urgency': urgency,
+        'date': date
+    };
+
 }
