e5be15c2906dc7741e298647f2ce921028753d3d
Rebecca2106 <you@example.com>
2022-04-10 11:29:20 +0200

    update-tasks

--- js/addtaskscript.js
+++ js/addtaskscript.js
@@ -1,10 +1,10 @@
+
+let allTasks=[];
 setURL('http://gruppe-221.developerakademie.net/smallest_backend_ever');
 
 
-let allTasks=[];
 
-
-function addTask(){
+function CreateTask(){
     let title = document.getElementById('title');
     let date= document.getElementById('date');
     let category = document.getElementById('category');
@@ -20,6 +20,47 @@ function addTask(){
         'date': date
     };
 
-    allTasks.push(task);
+    addTask(task);
 
 }
+
+function addTask(task){
+
+    allTasks.push(task);
+    backend.setItem('tasks', JSON.stringify(allTasks));
+    title.value='';
+    description.value='';
+    addTasksToServer(task);
+
+}
+
+async function addTasksToServer(allTasks) {
+    allTasks.push(task);
+    await saveJSONToServer('allTasks');
+}
+
+function DropdownList() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+imageInfos(prename, mail, picture)
+    let information = {
+        'firstname': prename,
+        
+        'Mail': mailadress,
+        'picture': bild
+    }
+
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
