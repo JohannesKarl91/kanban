1d211d8564d2c76eb53514428320357568c1cdeb
Johannes Weber <johannes.k.weber@web.de>
2022-04-11 21:17:06 +0200

    Debug responsive header and new general green layout

--- js/header.js
+++ js/header.js
@@ -1,43 +1,21 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("w3-include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
-        }
-      }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
-    }
-  }
-}
-
-
+/**
+ * Opens header as responsive version.
+ * @returns no return
+ */
 function openMobileMenu() {
-  document.getElementById('navbar').style = "display:block;";
+  document.getElementById('navbarResponsive').style = "display:block;";
   document.getElementById('mobileMenu-btn').innerHTML = "&#10006;"
   document.getElementById('mobileMenu-btn').onclick = closeMobileMenu;
   //Highlight mobileNav
   highlightNav("mobile" + saveNav);
 }
 
-
+/**
+ * Closes header as responsive version.
+ * @returns no return
+ */
 function closeMobileMenu() {
-  document.getElementById('navbar').style = "display:none;";
+  document.getElementById('navbarResponsive').style = "display:none;";
   document.getElementById('mobileMenu-btn').innerHTML = "&#9776;"
   document.getElementById('mobileMenu-btn').onclick = openMobileMenu;
 }
