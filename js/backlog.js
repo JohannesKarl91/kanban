4a3f7b43deeec62811b89f40f6161e0788447229
Johannes Weber <johannes.k.weber@web.de>
2022-04-13 14:26:12 +0200

    Update for backlog part

--- js/backlog.js
+++ js/backlog.js
@@ -1,28 +1,4 @@
-let backlogTask = [{
-    'title': 'Titel 1',
-    'category': 'Sales',
-    'description': 'Beschreibung 1',
-    'urgency': 'High',
-    'date': '14.04.2022',
-    'assigned': 'Alex Bachmann'
-},
-{
-    'title': 'Titel 2',
-    'category': 'IT',
-    'description': 'Beschreibung 2',
-    'urgency': 'Middle',
-    'date': '15.04.2022',
-    'assigned': 'Rebecca Häckl'
-},
-{
-    'title': 'Titel 3',
-    'category': 'Marketing',
-    'description': 'Beschreibung 3',
-    'urgency': 'Low',
-    'date': '16.04.2022',
-    'assigned': 'Johannes Weber'
-},
-];
+let backlogAddTask = [];
 
 /**
  * Check test array via console-log() function
@@ -33,9 +9,12 @@ function consoleCheck() {
 
 
 function renderBacklogItems() {
-    let backlogContent = document.getElementById('backlogContentTaskAsElement')
+    let backlogContent = document.getElementById('backlogContentTaskAsElement');
+}
 
-    if (backlogTask.length == 0) {
-        backlogContent.innerHTML = 'Add task'
+function CheckEmptyArray(){
+    
+    if (backlogAddTask.length == 0){
+        console.log('Array is empty!')
     }
 }
