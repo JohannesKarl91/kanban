b4cfc3327ef5a292ca2bcd2e9f64fe1471c02a3f
Johannes Weber <johannes.k.weber@web.de>
2022-04-13 12:16:16 +0200

    Add new JS-File backlog.js

---
+++ js/backlog.js
@@ -0,0 +1,25 @@
+let task=[{
+    'title': 'Titel 1',
+    'category': 'Sales',
+    'description': 'Beschreibung 1',
+    'urgency': 'High',
+    'date': '14.04.2022',
+    'assigned': 'Alex Bachmann'
+},
+{
+    'title': 'Titel 2',
+    'category': 'IT',
+    'description': 'Beschreibung 2',
+    'urgency': 'Middle',
+    'date': '15.04.2022',
+    'assigned': 'Rebecca Häckl'
+},
+{
+    'title': 'Titel 3',
+    'category': 'Marketing',
+    'description': 'Beschreibung 3',
+    'urgency': 'Low',
+    'date': '16.04.2022',
+    'assigned': 'Johannes Weber'
+},
+];
