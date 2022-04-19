d4c88de52dbc004d4334648ec620cb5c932561e6
Johannes Weber <johannes.k.weber@web.de>
2022-04-19 20:45:07 +0200

    Update to boardscript.js

--- js/boardscript.js
+++ js/boardscript.js
@@ -16,7 +16,7 @@ let testtask = [
         'description': 'Beschreibung 2',
         'urgency': 'Middle',
         'date': '15.04.2022',
-        'assigned': [3,2],
+        'assigned': [3, 2],
         'status': 'todo',
         'location': 'board',
     },
@@ -31,62 +31,62 @@ let testtask = [
         'location': 'board',
     }];
 
-    let boardtasks=[];
+let boardtasks = [];
 
-    async function initBoard() {
-        await initUsers();
-        await loadTasks();
-        includeHTML();
-        renderTaskstoBoard();
-    }
-
-function filtertask(board){   
-return tasks.filter(f => f.location == board)   //nahher wieder in tasks, ändern sucht alle trasks mit location board
+async function initBoard() {
+    await initUsers();
+    await loadTasks();
+    includeHTML();
+    renderTaskstoBoard();
 }
 
-function renderTaskstoBoard(){
-    let progresses= ['todo','inprogress','testing','done'];
-    boardtasks = filtertask('board'); 
+function filtertask(board) {
+    return tasks.filter(f => f.location == board)   //nahher wieder in tasks, ändern sucht alle trasks mit location board
+}
 
-    for (let status=0; status<progresses.length; status++){
+function renderTaskstoBoard() {
+    let progresses = ['todo', 'inprogress', 'testing', 'done'];
+    boardtasks = filtertask('board');
+
+    for (let status = 0; status < progresses.length; status++) {
         const progress = progresses[status];
-        let boardcolum= document.getElementById(progress);
-        boardcolum.innerHTML='';
-        for (let i=0; i<boardtasks.length; i++){
+        let boardcolum = document.getElementById(progress);
+        boardcolum.innerHTML = '';
+        for (let i = 0; i < boardtasks.length; i++) {
             const task = boardtasks[i];
-            if (task.status==progress){
-                boardcolum.innerHTML+=generateHTML(i,progress);
+            if (task.status == progress) {
+                boardcolum.innerHTML += generateHTML(i, progress);
                 colors(i);
                 taskassigned(i);
+            }
         }
     }
-    }
 }
-function colors(i){
-    let color =boardtasks[i].urgency
-    if (color== 'High'){
-        document.getElementById('header'+i).style='background-color: #de4e4e';
+function colors(i) {
+    let color = boardtasks[i].urgency
+    if (color == 'High') {
+        document.getElementById('header' + i).style = 'background-color: #de4e4e';
     }
-    if (color== 'Middle'){
-        document.getElementById('header'+i).style='background-color: #ed9e00';
+    if (color == 'Middle') {
+        document.getElementById('header' + i).style = 'background-color: #ed9e00';
     }
-    if (color== 'Low'){
-        document.getElementById('header'+i).style='background-color: #5376c9';
+    if (color == 'Low') {
+        document.getElementById('header' + i).style = 'background-color: #5376c9';
     }
 }
 
-function taskassigned(i){
-    for (let j=0; j<boardtasks[i].assigned.length; j++){
-        const assigned= boardtasks[i].assigned[j]['id'];
-        let user= users.filter(f => f.userId == assigned);
+function taskassigned(i) {
+    for (let j = 0; j < boardtasks[i].assigned.length; j++) {
+        const assigned = boardtasks[i].assigned[j]['id'];
+        let user = users.filter(f => f.userId == assigned);
         console.log("user", user);
-        document.getElementById('assigned'+i).innerHTML+=`<div class="user"><img class="member-img" src="${user[0]['profileimage']}"></img></div>`;
+        document.getElementById('assigned' + i).innerHTML += `<div class="user"><img class="member-img" src="${user[0]['profileimage']}"></img></div>`;
 
     }
 }
 
-function generateHTML(i,progress){
-    return` <div draggable="true" ondragstart="startDragging(${i})" class="task-card">
+function generateHTML(i, progress) {
+    return ` <div draggable="true" ondragstart="startDragging(${i})" class="task-card">
     <div id='header${i}' style="background-color: black" class="task-header">
         <div class="task-title">
             <h4>${boardtasks[i].title}</h4>
@@ -114,36 +114,42 @@ function generateHTML(i,progress){
 `;
 }
 
-function allowDrop(ev){
+function allowDrop(ev) {
     ev.preventDefault();
 }
 
-function startDragging(index){
-    currentDraggedElement=index;
+function startDragging(index) {
+    currentDraggedElement = index;
 }
 
-function moveto(newstatus){
-    boardtasks[currentDraggedElement].status=newstatus;
+function moveto(newstatus) {
+    boardtasks[currentDraggedElement].status = newstatus;
     updateBoardTasksToBackend();
     renderTaskstoBoard();
 }
 
-function deletetask(position){
-    boardtasks.splice(position,1)
-    updateBoardTasksToBackend();
-    renderTaskstoBoard()
+function deletetask(position) {
+    for (let index = 0; index < tasks.length; index++) {
+        if (boardtasks[position]['title'] == tasks[index]['title']) {
+
+            tasks.splice(position, 1)
+            updateBoardTasksToBackend();
+            renderTaskstoBoard()
+        }
+    }
 }
 
-function nextsection(position, progress){
-    currentDraggedElement=position;
-    if (progress=='todo'){
-        boardtasks[currentDraggedElement].status='inprogress'; 
+
+function nextsection(position, progress) {
+    currentDraggedElement = position;
+    if (progress == 'todo') {
+        boardtasks[currentDraggedElement].status = 'inprogress';
     }
-    if (progress=='inprogress'){
-        boardtasks[currentDraggedElement].status='testing'; 
+    if (progress == 'inprogress') {
+        boardtasks[currentDraggedElement].status = 'testing';
     }
-    if (progress=='testing'){
-        boardtasks[currentDraggedElement].status='done'; 
+    if (progress == 'testing') {
+        boardtasks[currentDraggedElement].status = 'done';
     }
     updateBoardTasksToBackend();
     renderTaskstoBoard();
