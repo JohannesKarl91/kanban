6dd8d4b822919716286fa858297b9a75191df303
Rebecca2106 <you@example.com>
2022-04-19 13:45:28 +0200

    change-color-board

--- js/boardscript.js
+++ js/boardscript.js
@@ -8,6 +8,7 @@ let testtask = [
         'date': '14.04.2022',
         'assigned': [1],
         'status': 'todo',
+        'location': 'board',
     },
     {
         'title': 'Titel 2',
@@ -30,6 +31,8 @@ let testtask = [
         'location': 'board',
     }];
 
+    let boardttasks=[];
+
     async function initBoard() {
         await initUsers();
         await loadTasks();
@@ -43,14 +46,14 @@ return testtask.filter(f => f.location == board)   //nahher wieder in sortTasks,
 
 function renderTaskstoBoard(){
     let progresses= ['todo','inprogress','testing','done'];
-    // let boardttasks = filtertask('board'); für später zum fikter, welche tasks aufs board sollen
+    boardttasks = filtertask('board'); 
 
     for (let status=0; status<progresses.length; status++){
         const progress = progresses[status];
         let boardcolum= document.getElementById(progress);
         boardcolum.innerHTML='';
-        for (let i=0; i<testtask.length; i++){
-            const task = testtask[i];
+        for (let i=0; i<boardttasks.length; i++){
+            const task = boardttasks[i];
             if (task.status==progress){
                 boardcolum.innerHTML+=generateHTML(i,progress);
                 colors(i);
@@ -60,7 +63,7 @@ function renderTaskstoBoard(){
     }
 }
 function colors(i){
-    let color =testtask[i].urgency
+    let color =boardttasks[i].urgency
     if (color== 'High'){
         document.getElementById('header'+i).style='background-color: #de4e4e'
     }
@@ -73,8 +76,8 @@ function colors(i){
 }
 
 function taskassigned(i){
-    for (let j=0; j<testtask[i].assigned.length; j++){
-        const assigned= testtask[i].assigned[j];
+    for (let j=0; j<boardttasks[i].assigned.length; j++){
+        const assigned= boardttasks[i].assigned[j];
         let user= users.filter(f => f.userId == assigned);
         // console.log(user);
         document.getElementById('assigned'+i).innerHTML+=`<div class="user"><img class="member-img" src="${user[0]['profileimage']}"></img></div>`;
@@ -86,23 +89,23 @@ function generateHTML(i,progress){
     return` <div draggable="true" ondragstart="startDragging(${i})" class="task-card">
     <div id='header${i}' style="background-color: black" class="task-header">
         <div class="task-title">
-            <h4>${testtask[i].title}</h4>
+            <h4>${boardttasks[i].title}</h4>
         </div>
         <div class="task-delete-btn"><span onclick="deletetask(${i})">&#x1F5D1;</span></div>
     </div>
     <div class="task-meta-info">
         <div class="task-duedate">
             <img src="./img/icons8-calendar-150.png" class="calendar-img">
-            <span>${testtask[i].date}</span>
+            <span>${boardttasks[i].date}</span>
         </div>
         <div class="task-assigned" id="assigned${i}">
         </div>
     </div>
     <div class="task-description">
-        <span>${testtask[i].description}</span>
+        <span>${boardttasks[i].description}</span>
     </div>
     <div class="task-footer">
-        <div class="task-category"><span>${testtask[i].category}</span></div>
+        <div class="task-category"><span>${boardttasks[i].category}</span></div>
         <div class="task-action-btn" onclick="nextsection(${i},'${progress}')">
             <span>&#10149;</span>
         </div>
@@ -120,25 +123,25 @@ function startDragging(index){
 }
 
 function moveto(newstatus){
-    testtask[currentDraggedElement].status=newstatus;
+    boardttasks[currentDraggedElement].status=newstatus;
     renderTaskstoBoard();
 }
 
 function deletetask(position){
-    testtask.splice(position,1)
+    boardttasks.splice(position,1)
     renderTaskstoBoard()
 }
 
 function nextsection(position, progress){
     currentDraggedElement=position;
     if (progress=='todo'){
-        testtask[currentDraggedElement].status='inprogress'; 
+        boardttasks[currentDraggedElement].status='inprogress'; 
     }
     if (progress=='inprogress'){
-        testtask[currentDraggedElement].status='testing'; 
+        boardttasks[currentDraggedElement].status='testing'; 
     }
     if (progress=='testing'){
-        testtask[currentDraggedElement].status='done'; 
+        boardttasks[currentDraggedElement].status='done'; 
     }
 
     renderTaskstoBoard();
