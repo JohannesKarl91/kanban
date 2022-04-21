d74dc0a06f5ef1d07473a686946ce4c151ddbd33
Johannes Weber <johannes.k.weber@web.de>
2022-04-21 18:00:47 +0200

    Implementation of backlog Feedback by sending a task to the KANBAN board.

--- js/boardscript.js
+++ js/boardscript.js
@@ -134,7 +134,7 @@ function openedit(i){
 }
 
 function editor(i){
-return`<div id="taskat${i}" draggable="true" ondragstart="startDragging(${i})" class="task-card">
+return`<div id="taskat${i}" draggable="true" ondragstart="startDragging(${i})" class="task-card-edit">
     <div class="column" id='header${i}' style="background-color:" class="task-header">
         <div class="task-title">
             <input id="title_edit${i}" type="text" style="border-radius: 5px;" placeholder="Bitte Titel eingeben" value='${boardtasks[i].title}'} 
