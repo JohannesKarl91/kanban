9c3770639872c35ac12c04404826a89f319b9f90
Johannes Weber <johannes.k.weber@web.de>
2022-04-22 15:15:37 +0200

    Debugging and change from allTasks- to tasks-array in board section

--- js/boardscript.js
+++ js/boardscript.js
@@ -20,12 +20,12 @@ function renderTaskstoBoard() {
     document.getElementById('testing').innerHTML='';
     document.getElementById('done').innerHTML='';
     // boardtasks = filtertask('board');
-    for (let i = 0; i < allTasks.length; i++) {
-        if (allTasks[i].location == "board")
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].location == "board")
             for (let status = 0; status < progresses.length; status++) {
                 const progress = progresses[status];
                 let boardcolum = document.getElementById(progress);
-                    const task = allTasks[i];
+                    const task = tasks[i];
                     if (task.status == progress) {
                         boardcolum.innerHTML += generateHTML(i, progress);
                         colors(i);
@@ -36,7 +36,7 @@ function renderTaskstoBoard() {
     }
 
     function colors(i) {
-        let color = allTasks[i].urgency
+        let color = tasks[i].urgency
         if (color == 'High') {
             document.getElementById('header' + i).style = 'background-color: #de4e4e';
         }
@@ -49,10 +49,10 @@ function renderTaskstoBoard() {
     }
 
     function taskassigned(i) {
-        for (let j = 0; j < allTasks[i].assigned.length; j++) {
-            const assigned = allTasks[i].assigned[j]['id'];
+        for (let j = 0; j < tasks[i].assigned.length; j++) {
+            const assigned = tasks[i].assigned[j]['id'];
             let user = users.filter(f => f.userId == assigned);
-            console.log("user", user);
+            //console.log("user", user);
             document.getElementById('assigned' + i).innerHTML += `<div class="user"><img class="member-img" src="${user[0]['profileimage']}"></img></div>`;
 
         }
@@ -62,23 +62,23 @@ function renderTaskstoBoard() {
         return ` <div id="taskat${i}" draggable="true" ondragstart="startDragging(${i})" class="task-card">
     <div id='header${i}' style="background-color: black" class="task-header">
         <div class="task-title">
-            <h4>${allTasks[i].title}</h4>
+            <h4>${tasks[i].title}</h4>
         </div>
         <div class="task-delete-btn"><span onclick="deletetask(${i})">&#x1F5D1;</span></div>
     </div>
     <div class="task-meta-info">
         <div class="task-duedate">
             <img src="./img/icons8-calendar-150.png" class="calendar-img">
-            <span>${allTasks[i].date}</span>
+            <span>${tasks[i].date}</span>
         </div>
         <div class="task-assigned" id="assigned${i}">
         </div>
     </div>
     <div class="task-description">
-        <span>${allTasks[i].description}</span>
+        <span>${tasks[i].description}</span>
     </div>
     <div class="task-footer">
-        <div class="task-category"><span>${allTasks[i].category}</span></div>
+        <div class="task-category"><span>${tasks[i].category}</span></div>
         <div class="boardBtnSection">
             <img onclick="openedit(${i})" style="width: 20px; height: 20px;" class="member-img" src="./img/edit.svg">
             <div class="task-action-btn" onclick="nextsection(${i},'${progress}')">
@@ -99,13 +99,13 @@ function renderTaskstoBoard() {
     }
 
     function moveto(newstatus) {
-        allTasks[currentDraggedElement].status = newstatus;
+        tasks[currentDraggedElement].status = newstatus;
         updateBoardTasksToBackend();
         renderTaskstoBoard();
     }
 
     function deletetask(position) {
-        allTasks.splice(position,1)
+        tasks.splice(position,1)
                 updateBoardTasksToBackend();
                 renderTaskstoBoard()
             
@@ -116,16 +116,16 @@ function renderTaskstoBoard() {
     function nextsection(position, progress) {
         currentDraggedElement = position;
         if (progress == 'todo') {
-            allTasks[currentDraggedElement].status = 'inprogress';
+            tasks[currentDraggedElement].status = 'inprogress';
         }
         if (progress == 'inprogress') {
-            allTasks[currentDraggedElement].status = 'testing';
+            tasks[currentDraggedElement].status = 'testing';
         }
         if (progress == 'testing') {
-            allTasks[currentDraggedElement].status = 'done';
+            tasks[currentDraggedElement].status = 'done';
         }
         if (progress == 'done') {
-            allTasks[currentDraggedElement].status = 'todo';
+            tasks[currentDraggedElement].status = 'todo';
         }
         updateBoardTasksToBackend();
         renderTaskstoBoard();
@@ -142,19 +142,19 @@ function renderTaskstoBoard() {
         return `<div id="taskat${i}" draggable="true" ondragstart="startDragging(${i})" class="task-card-edit">
     <div class="column" id='header${i}' style="background-color:" class="task-header">
         <div class="task-title">
-            <input id="title_edit${i}" type="text" style="border-radius: 5px;" placeholder="Bitte Titel eingeben" value='${allTasks[i].title}'} 
+            <input id="title_edit${i}" type="text" style="border-radius: 5px;" placeholder="Bitte Titel eingeben" value='${tasks[i].title}'} 
         </div>
     </div>
     <div class="task-meta-info">
         <div class="task-duedate">
-            <input onclick="duedate2(${i})" style="border-radius: 5px;" value=${allTasks[i].date} class="relative bgr-input" type="date" id="editdate${i}" min="">
+            <input onclick="duedate2(${i})" style="border-radius: 5px;" value=${tasks[i].date} class="relative bgr-input" type="date" id="editdate${i}" min="">
         </div>
         <div class="task-assigned" id="assigned${i}">
         </div>
     </div>
-    <textarea style="border-radius: 5px;" id="description_edit${i}" rows="5">${allTasks[i].description}</textarea>
+    <textarea style="border-radius: 5px;" id="description_edit${i}" rows="5">${tasks[i].description}</textarea>
     <div class="task-footer">
-                        <select style="border-radius: 5px;" class="input relative" value="${allTasks[i].category}" id="category_change${i}">
+                        <select style="border-radius: 5px;" class="input relative" value="${tasks[i].category}" id="category_change${i}">
                         </select>
         <div class="task-action-btn"> <button style="border-radius: 5px; background-color: rgb(93, 156, 112);" onclick="changetask(${i})" <span class="material-symbols-outlined">
         edit
@@ -167,20 +167,20 @@ function renderTaskstoBoard() {
 
     function changetask(i) {
         let editTitle = document.getElementById('title_edit'+ i).value;
-        allTasks[i].title = editTitle;
+        tasks[i].title = editTitle;
         let editDate = document.getElementById('editdate'+i).value;
-        allTasks[i].date=editDate;
+        tasks[i].date=editDate;
         let editDescription= document.getElementById('description_edit'+i).value;
-        allTasks[i].description= editDescription;
+        tasks[i].description= editDescription;
         let editCategory=document.getElementById('category_change'+i).value;
-        allTasks[i].category=editCategory
+        tasks[i].category=editCategory
         renderTaskstoBoard()
     }
 
 
     function renderCategory(i) {
         let categories = ['Marketing', 'Sale', 'IT']
-        let selectedcategory = allTasks[i].category;
+        let selectedcategory = tasks[i].category;
         let html = document.getElementById('category_change' + i);
 
         for (let j = 0; j < categories.length; j++) {
@@ -201,8 +201,8 @@ function renderTaskstoBoard() {
 }
 
     function checkassign(i,k){
-        for (let j = 0; j < allTasks[i].assigned.length; j++) {
-        if (users[k].userId == allTasks[i].assigned[j]['id'])
+        for (let j = 0; j < tasks[i].assigned.length; j++) {
+        if (users[k].userId == tasks[i].assigned[j]['id'])
                 document.getElementById('user' + k).classList.add('edit')
     }
 }
@@ -211,14 +211,14 @@ function changeassign(i,k){}
 // if user ist assigned methode delete else add
 
     //     let assign=[1,2,3,4]
-    //     let assigntotask= allTasks[i].assigned
+    //     let assigntotask= tasks[i].assigned
     //     for(let k=0; k< assigntotask.length; k++){
     //         for(let j = 0; j < assign.length; j++) {
     //             if(assigntotask[k] == assign[j])
     //             document.getElementById('assigned'+i).innerHTML 
     //     }
-    //     for (let j = 0; j < allTasks[i].assigned.length; j++) {
-    //         const assigned = allTasks[i].assigned[j]['id'];
+    //     for (let j = 0; j < tasks[i].assigned.length; j++) {
+    //         const assigned = tasks[i].assigned[j]['id'];
     //         let user = users.filter(f => f.userId == assigned);
     //         document.getElementById('assigned' + i).innerHTML += `<div class="user"><img class="member-img" src="${user[0]['profileimage']}"></img></div>`;
     //         document.getElementById('assigned'+i).innerHTML 
